5641c1093c5c179b299a81ae22cee7490c192e5a
Siddhi292005 <siddhidambe29@gmail.com>
2024-12-26 16:51:53 +0530

    Create program3.cpp

---
+++ program3.cpp
@@ -0,0 +1,13 @@
+#include<iostream.h>
+using namespace std;
+int main()
+{
+  int n, fact=1;
+  cout<<"enter a number";
+  cin>>n;
+  for(int i=1; i<=n; i++)
+    {
+      fact*=i;
+    }
+  cout<<fact;
+}
